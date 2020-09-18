ccd49bc6bb11c899b877b83db3f7ecd07f6b3dfb
Jam Risser <jam@codejam.ninja>
2020-09-18 01:30:07 +0000

    Added pnpm support

--- src/install.js
+++ src/install.js
@@ -1,4 +1,6 @@
 import execa from 'execa';
+import mapSeriesAsync from 'map-series-async';
+import which from 'which';
 
 export default async function install(yo) {
   await execa('git', ['init'], {
@@ -10,9 +12,25 @@ export default async function install(yo) {
   if (!yo.answers.install || installChar === 'n' || installChar === 'f') {
     return false;
   }
+  await npmInstall(yo, (await getCommand()) || 'npm');
   return yo.installDependencies({
-    npm: true,
+    npm: false,
     bower: false,
     yarn: false
   });
+}
+
+export async function npmInstall(yo, npm = 'npm') {
+  await execa(npm, ['install'], {
+    cwd: yo.destinationPath(''),
+    stdio: 'inherit'
+  });
+}
+
+export async function getCommand(commands = ['pnpm', 'yarn', 'npm']) {
+  let result = null;
+  await mapSeriesAsync(commands, async (command) => {
+    if (!result && (await which(command))) result = command;
+  });
+  return result;
 }
